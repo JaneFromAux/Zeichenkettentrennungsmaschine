f25921a00530a0cdd69d7a35a99ccb693b9068cf
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-19 16:58:26 +0200

    js runs

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+let inputText = document.getElementById('inputText');
+let inputTrenn = document.getElementById('inputTrenn');
+let davorTrenn = document.getElementById('davorTrenn');
+let danachTrenn = document.getElementById('danachTrenn');
+let outputVorne = document.getElementById('outputVorne');
+let outputHinten = document.getElementById('outputHinten');
+
+
+
+let trennTrenn = () => {
+    console.log('trennTrenn trennt');
+
+    let stringKette = inputText.value.split(" ");
+    let position = inputTrenn.value;
+    console.log(stringKette);
+    let schnittstelle = stringKette.indexOf(position);
+
+    console.log(stringKette.indexOf(position));
+
+    if (davorTrenn.checked && schnittstelle > 0) {
+        console.log('davorTrenn');
+
+        let ausgabeDavor = stringKette.splice(0, schnittstelle);
+        console.log(ausgabeDavor.join(" "));
+        outputVorne.innerHTML = ausgabeDavor.join(" ");
+
+        console.log(stringKette.join(" "));
+        outputHinten.innerHTML = stringKette.join(" ");
+
+    } else if (danachTrenn.checked && schnittstelle > 0) {
+        console.log('danachTrenn');
+
+        let ausgabeDanach = stringKette.splice((schnittstelle + 1));
+        console.log(ausgabeDanach.join(" "));
+        outputHinten.innerHTML = ausgabeDanach.join(" ");
+        outputVorne.innerHTML = stringKette.join(" ");
+
+    } else if (davorTrenn.checked && schnittstelle === -1) {
+        outputVorne.innerHTML = "Das Zeichen konnte leider nicht gefunden werden";
+        outputHinten.innerHTML = inputText.value;
+    }
+    else if (danachTrenn.checked && schnittstelle === -1) {
+        outputVorne.innerHTML = inputText.value;
+        outputHinten.innerHTML = "Das Zeichen konnte leider nicht gefunden werden";
+    }
+
+}
